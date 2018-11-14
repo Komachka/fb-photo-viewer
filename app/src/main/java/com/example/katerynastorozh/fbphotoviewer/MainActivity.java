cfc151c06880b5efb8dd53f28e5cd90a89c4f5ab
Kateryna Strorozh <kateryna.storozh@trinetix.com>
2018-11-14 19:21:00 +0200

    Add login button

--- app/src/main/java/com/example/katerynastorozh/fbphotoviewer/MainActivity.java
+++ app/src/main/java/com/example/katerynastorozh/fbphotoviewer/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.katerynastorozh.fbphotoviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    public CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        launchAccountActivity();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void launchAccountActivity() {
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
